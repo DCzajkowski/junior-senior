63864aa1149f02883738ab11a49c2e51bcc328c9
Dariusz Czajkowski <darek@darek.dev>
2021-04-13 19:19:41 +0200

    Improved initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export function App(): JSX.Element {
-  return <div className="md:hyphens-auto">Hello, World!</div>
+  return <div>Hello, World!</div>
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import '../dist/tailwind.css'
 import { App } from './App'
 
 ReactDOM.render(
